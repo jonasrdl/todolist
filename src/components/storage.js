dc25354998b58c213e44aef808f7b19f624f126a
jriedel <jriedel@united-internet.de>
2021-05-05 15:05:29 +0200

    Added Storage class, and first functions

---
+++ src/components/storage.js
@@ -0,0 +1,17 @@
+class Storage {
+  constructor(key) {
+    this.key = key; // Localstorage key
+  }
+
+  setData(data) {
+    localStorage.setItem(this.key, JSON.stringify(data)); //
+  }
+
+  getData() {
+    localStorage.getItem(this.key);
+  }
+
+  clearData() {
+    localStorage.removeItem(this.key);
+  }
+}
