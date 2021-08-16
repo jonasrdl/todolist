f0c75f532dbd3107980aba1b245f1d9b42b24558
jonasrdl <jonasriedel@gmx.net>
2021-08-16 20:43:52 +0200

    Formatted

--- src/components/Pagination.js
+++ src/components/Pagination.js
@@ -1,33 +1,33 @@
-import Eventbus from "../../eventbus.js";
+import Eventbus from '../../eventbus.js';
 
 export class Pagination {
-    constructor(length, currentPage = 0) {
-        this.maxPages = length - 1;
-        this.currentPage = currentPage;
+  constructor(length, currentPage = 0) {
+    this.maxPages = length - 1;
+    this.currentPage = currentPage;
+  }
+
+  setPage(index) {
+    if (index > this.maxPages + 1 || index < 0) {
+      return;
     }
 
-    setPage(index) {
-        if (index > this.maxPages + 1 || index < 0) {
-            return;
-        }
+    this.currentPage = index;
+    Eventbus.emit('pageChange', index);
+  }
 
-        this.currentPage = index;
-        Eventbus.emit('pageChange', index);
-    }
+  changePage(direction) {
+    this.setPage(this.currentPage + direction);
+  }
 
-    changePage(direction) {
-        this.setPage(this.currentPage + direction);
-    };
+  prevPage() {
+    this.changePage(-1);
+  }
 
-    prevPage() {
-        this.changePage(-1);
-    }
+  nextPage() {
+    this.changePage(1);
+  }
 
-    nextPage() {
-        this.changePage(1);
-    }
-
-    addMaxPage(pages = 1) {
-        this.maxPages += pages;
-    }
+  addMaxPage(pages = 1) {
+    this.maxPages += pages;
+  }
 }
