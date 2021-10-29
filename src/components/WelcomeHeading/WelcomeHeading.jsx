49746579163977387572ab4a0605b18dd1ee7d01
Jonas <jonasriedel@protonmail.com>
2021-10-29 17:50:58 +0200

    Added WelcomeHeading component

--- src/components/WelcomeHeading/WelcomeHeading.jsx
+++ src/components/WelcomeHeading/WelcomeHeading.jsx
@@ -4,7 +4,7 @@ import './WelcomeHeading.css'
 const WelcomeHeading = () => {
   return (
     <>
-      <h2 className={"welcome-heading"}>Hey <input type="text" className="name-input input-styling" /><span className="welcome-name">[NAME]!</span></h2>
+      <h2 className={"welcome-heading"}>Hey <span className="welcome-name">[USERNAME]!</span></h2>
       <span className={"welcome-heading-text"}>You currently have <span className="marked-list-text">0 lists</span> with a total of <span className="marked-todo-text">0 todos.</span></span>
     </>
   )
