76003d653d1b0c4ba02f6f167c80a5145ac5ad02
Jonas <jonasriedel@protonmail.com>
2021-11-03 20:58:31 +0100

    Move file

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react'
 import './App.css'
 import TodoForm from './components/TodoForm/TodoForm'
-import TodoList from './components/TodoList'
+import TodoList from './components/TodoList/TodoList'
 import Navbar from './components/Navbar/Navbar'
 import WelcomeHeading from './components/WelcomeHeading/WelcomeHeading'
 
